e1798dcfe8ce83a1c88b1997f65e7d1ee9e8860e
OUlmachi-Yassir <oulmachi.yassir@gmail.com>
2024-11-06 13:00:17 +0100

    add: Frontend/sign-up et login

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './pages/Users/Main';
+import Login from './pages/auth/login';
+import SignUp from './pages/auth/register';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/users" element={<Main />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        {/* <Route path="*" element={<NoPage />} /> */}
       </Routes>
     </Router>
   );

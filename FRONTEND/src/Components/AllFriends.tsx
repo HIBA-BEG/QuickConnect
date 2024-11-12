3e00240e27993a4ecfa97afdfe0ee93ec74d5464
HIBA BEGHDI <beghiba@gmail.com>
2024-11-12 17:01:28 +0100

    QUICK-5 : Implemented UserSearchPopup component with search functionality, creation of the component, fetching the data from the api, changed the default profile picture in case it doesn't exist

--- FRONTEND/src/Components/AllFriends.tsx
+++ FRONTEND/src/Components/AllFriends.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
-import { FaSearch, FaTrashAlt } from 'react-icons/fa';
 import React, { useState } from 'react';
+import SearchBarComponent from './SearchBar';
+import defaultProfileIcon from '../profileicon.jpg';
 
 const Friends = styled.div`
   display: flex;
@@ -8,39 +9,6 @@ const Friends = styled.div`
   max-width: 300px;
   width: 90%;
   height: 100vh;
-`;
-
-const SearchBar = styled.div`
-  display: flex;
-  align-items: center;
-  background-color: #DBDCFF;
-  padding: 10px;
-  border-radius: 20px;
-  margin: 10px 0;
-  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
-`;
-
-const IconWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 5px;
-  color: #333;
-`;
-
-const Input = styled.input`
-  flex: 1;
-  padding: 3px;
-  border: none;
-  outline: none;
-  background-color: transparent;
-  font-size: 16px;
-`;
-
-const Divider = styled.div`
-  width: 1px;
-  height: 20px;
-  background-color: #999;
-  margin: 0 10px;
 `;
 
 const FriendItem = styled.div<{ active?: boolean }>`
@@ -84,23 +52,14 @@ const AllChatFiends: React.FC<AllChatProps> = ({ onFriendselect }) => {
 
   return (
     <Friends>
-      <SearchBar>
-        <IconWrapper>
-          <FaSearch size={18} />
-        </IconWrapper>
-        <Input type="text" placeholder="Search for New friends" />
-        <Divider />
-        <IconWrapper>
-          <FaTrashAlt size={18} />
-        </IconWrapper>
-      </SearchBar>
+      <SearchBarComponent />
       {['Friend 1', 'Friend 2', 'Friend 3'].map((FriendName, index) => (
         <FriendItem
           key={index}
           onClick={() => handleFriendselect(FriendName)} 
           active={activeGroup === FriendName} 
         >
-          <ProfileImage src="https://via.placeholder.com/40" alt={`${FriendName} Profile`} />
+          <ProfileImage src={defaultProfileIcon} alt={`${FriendName} Profile`} />
           <GroupInfo>
             <h2>{FriendName}</h2>
             <p>Last Message: Hello</p>

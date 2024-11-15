46773de31f9e4a17ef8f3c225d759f3b44f50023
IBRA-oub <brahimbourih@gmail.com>
2024-11-15 14:34:46 +0100

    feat : fonction de deleted channel

--- FRONTEND/src/Components/channelComponents/ChannelInfo.tsx
+++ FRONTEND/src/Components/channelComponents/ChannelInfo.tsx
@@ -4,8 +4,10 @@ import { Channel } from '../../Types/Channel';
 import { editeChannel } from '../../Api/channelAPI/editeChannelApi';
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
+import PopUp from '../PopUp';
+import { deleteChannel } from '../../Api/channelAPI/deleteChannelApi';
 interface props {
-    channelInfo: Channel[];
+    channelInfo: Channel[];  
 }
 export default function ChannelInfo({ channelInfo }: props) {
 
@@ -13,8 +15,11 @@ export default function ChannelInfo({ channelInfo }: props) {
     const [options, setOptions] = useState(false);
     const [isEditable, setIsEditable] = useState(false);
     const [editeButton, setEditeButton] = useState(false);
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [toDelete, setToDelete] = useState<string | number>(''); 
+
     // =============================data update =========================
-  
+
 
     type ChannelType = "Private" | "Public" | "Conversation";
     const [data, setData] = useState({
@@ -48,11 +53,29 @@ export default function ChannelInfo({ channelInfo }: props) {
     const handleUpdate = async (e: React.FormEvent) => {
         e.preventDefault();
         console.log(data.expirationTime);
-        
+
         await editeChannel(data, channelInfo[0]?._id);
         toast.success('Channel updated successfully!', { position: 'bottom-right' });
     };
-    // =======================================================
+    // ============================ handle group to delete ===========================
+
+    const handleOpenPopup = (id:string | number) => {
+        setToDelete(id)
+        setIsPopupOpen(true);
+      };
+
+      const handleClosePopup = () => {
+        setIsPopupOpen(false);
+      };
+
+      const handleSubmit = async() => {
+        await deleteChannel(toDelete)
+        setIsPopupOpen(false); 
+        toast.success('Channel deleted successfully!', { position: 'bottom-right' });
+      };
+
+    //   ====================================
+
     const toggleOptions = () => {
         setOptions(!options);
         setEditeButton(false);
@@ -90,7 +113,7 @@ export default function ChannelInfo({ channelInfo }: props) {
                             <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><path fill="#00ff04" d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-96c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 64z" /></svg>
 
                         </div>
-                        <div className='cursor-pointer'>
+                        <div onClick={() =>handleOpenPopup(channelInfo[0]?._id)} className='cursor-pointer'>
                             <svg xmlns="http://www.w3.org/2000/svg" height="20" width="17.5" viewBox="0 0 448 512"><path fill="#ff0000" d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z" /></svg>
 
                         </div>
@@ -134,7 +157,7 @@ export default function ChannelInfo({ channelInfo }: props) {
                         <label className="block text-black font-semibold mb-2">Banne World</label>
                         <textarea disabled={!isEditable} name="bannedWords" value={data.bannedWords} onChange={handleInputChange}
                             className="bg-indigo-100 text-gray-700 p-3 rounded-md max-h-44 w-[100%]">
-                            
+
                         </textarea>
                     </div>
                     {editeButton && <div className='w-full flex justify-end'>
@@ -147,6 +170,11 @@ export default function ChannelInfo({ channelInfo }: props) {
 
                 </form>
             </div>
+            <PopUp
+                isOpen={isPopupOpen}
+                onClose={handleClosePopup}
+                onSubmit={handleSubmit}
+            />
             <ToastContainer />
         </div >
     )

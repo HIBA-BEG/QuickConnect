0d485c08c7a2825dbb6782eef406fdb31723f477
OUlmachi-Yassir <oulmachi.yassir@gmail.com>
2024-11-17 01:53:51 +0100

    add: macking messages between freinds and the loged user with amplimanting websocket for live conversation

--- BACKEND/src/app.module.ts
+++ BACKEND/src/app.module.ts
@@ -12,6 +12,8 @@ import { BanListModule } from './ban-list/ban-list.module';
 import { RatingModule } from './rating/rating.module';
 import { RewardModule } from './reward/reward.module';
 import { VideoCallGateway } from './video-call/video-call.gateway';
+import { ConversationsModule } from './conversation/conversation.module';
+import { ChatGateway } from './socket/socket.gateway';
 
 @Module({
   imports: [
@@ -28,8 +30,9 @@ import { VideoCallGateway } from './video-call/video-call.gateway';
     BanListModule,
     RatingModule,
     RewardModule,
+    ConversationsModule,
   ],
   controllers: [AppController],
-  providers: [AppService,VideoCallGateway],
+  providers: [AppService,VideoCallGateway,ChatGateway],
 })
 export class AppModule {}
